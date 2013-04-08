3720eba8986c2ebc9391bc747953819a2d6dd8a2
Eric Bitzegaio <ericbitzegaio@pham.home>
2013-04-08 15:34:00 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vidscreen::Application.config.secret_token = '3b74f88d54969f42f8b5528b2c844ca6a2a3b38f1f5b4d6840cd49fcc580e8f859dae2d9a0e3370f623b027ec36eedb57391cc1053f58a961d740d08df3853ba'
